ce89a8351ff1a3c18a8db6f4e13b43d1cf92764d
Rahmatshoh <rahmatshoh.rahmonov@mail.ru>
2022-04-11 16:35:18 +0500

    tailwind setting

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-red-500">HEKKKFEF</h1>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
 
